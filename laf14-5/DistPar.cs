1ded5b5ff4c0ede1f7332c8c490b29670de34b48
Andrey Denisenko <francescogacczi@gmail.com>
2018-09-20 16:37:10 +0300

    chng

---
+++ laf14-5/DistPar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laf14_5
+{
+    class DistPar
+    {
+        public int distance;   // distance from start to this vertex
+        public int parentVert; // current parent of this vertex
+        public DistPar(int pv, int d)  // constructor
+        {
+            distance = d;
+            parentVert = pv;
+        }
+    }
+}
---
+++ laf14-5/Vertex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laf14_5
+{
+    class Vertex
+    {
+        public char label;        // label (e.g. 'A')
+        public bool isInTree;
+        public Vertex(char lab)   // constructor
+        {
+            label = lab;
+            isInTree = false;
+        }
+    }
+}
