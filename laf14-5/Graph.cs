4266d9f1572cc964af69001a91691db0c7317399
Andrey Denisenko <francescogacczi@gmail.com>
2018-09-20 17:20:01 +0300

    chng2

--- laf14-5/Graph.cs
+++ laf14-5/Graph.cs
@@ -8,5 +8,83 @@ namespace laf14_5
 {
     class Graph
     {
+        private readonly int MAX_VERTS = 20;
+        private readonly int INFINITY = 1000000;
+        private Vertex[] vertexList; // list of vertices
+        private int[,] adjMat;      // adjacency matrix
+        private int nVerts;          // current number of vertices
+        private int[] vertexs;		// array for index of vertex in vertexList
+        public Graph()               // constructor
+        {
+            vertexList = new Vertex[MAX_VERTS];
+            // adjacency matrix
+            adjMat = new int[MAX_VERTS, MAX_VERTS];
+            nVerts = 0;
+            for (int j = 0; j < MAX_VERTS; j++)
+                // set adjacency
+                for (int k = 0; k < MAX_VERTS; k++)
+                    // matrix
+                    adjMat[j, k] = INFINITY;     // to infinity
+        }  // end constructor
+        public void addVertex(char lab)
+        {
+            vertexList[nVerts++] = new Vertex(lab);
+        }
+        public void addEdge(int start, int end, int weight)
+        {
+            adjMat[start, end] = weight;  // (directed)
+            adjMat[end, start] = weight;  // (directed)
+        }
+        public void displayWord()
+        {
+            for (int j = 0; j < nVerts; j++)
+            {
+                Console.Write(" " + vertexList[vertexs[j]].label);
+            }
+            Console.WriteLine(" " + vertexList[vertexs[0]].label);
+        }
+        public void travelingSalesman()
+        {
+            vertexs = new int[nVerts];
+            for (int i = 0; i < nVerts; i++)
+            {
+                vertexs[i] = i;
+            }
+            doAnagram(vertexs.Length);
+        }
+        public void doAnagram(int newSize)
+        {
+            if (newSize == 1)
+            {                    // if too small,
+                for (int i = 0; i < nVerts - 1; i++)
+                {
+                    if (adjMat[vertexs[i],vertexs[i + 1]] == INFINITY)
+                    {
+                        return;
+                    }
+                }
+                if (adjMat[vertexs[nVerts - 1],vertexs[0]] != INFINITY)
+                {
+                    displayWord();
+                }
+                return;
+            }                          // go no further
+            for (int j = 0; j < newSize; j++)         // for each position,
+            {
+                doAnagram(newSize - 1);             // anagram remaining
+                rotate(newSize);
+            }
+        }
+        public void rotate(int newSize)
+        // rotate left all chars from position to end
+        {
+            int j;
+            int position = nVerts - newSize;
+            int temp = vertexs[position];       // save first letter
+            for (j = position + 1; j < nVerts; j++)
+                // shift others left
+                vertexs[j - 1] = vertexs[j];
+            vertexs[j - 1] = temp;                 // put first on right
+        }
     }
 }
--- laf14-5/Program.cs
+++ laf14-5/Program.cs
@@ -10,6 +10,26 @@ namespace laf14_5
     {
         static void Main(string[] args)
         {
+            Graph theGraph = new Graph();
+            theGraph.addVertex('A'); // 0 (start)
+            theGraph.addVertex('B'); // 1
+            theGraph.addVertex('C'); // 2
+            theGraph.addVertex('D'); // 3
+            theGraph.addVertex('E'); // 4
+
+            theGraph.addEdge(0, 1, 50); // AB 50
+            theGraph.addEdge(0, 2, 50); // AC 50
+            theGraph.addEdge(1, 3, 90); // BD 90
+            theGraph.addEdge(2, 3, 40); // CD 40
+            theGraph.addEdge(3, 4, 20); // DE 20
+            theGraph.addEdge(4, 0, 70); // EA 70
+            theGraph.addEdge(4, 1, 50); // EB 50
+
+            theGraph.travelingSalesman();
+            theGraph.displayWord();
+            Console.WriteLine("over!");
+
+            Console.ReadLine();
         }
     }
 }
